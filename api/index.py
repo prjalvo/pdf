732bf62873bc349b5f4cdbe3dcbe5ee3dfe871b4
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-06-24 21:10:48 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,3 @@
-from docx2pdf import convert
-import os
-
 require('dotenv').config();
 
 const express = require('express');
@@ -16,32 +13,7 @@ app.use(express.static('public'));
 
 app.post('/convert', urlencodedParser, async (req, res) => {
 	try {
-		 # Verifica se foi enviado um arquivo DOCX
-        if 'file' not in request.files:
-            return "Erro: Nenhum arquivo enviado.", 400
-
-        file = request.files['file']
-
-        # Verifica se o arquivo tem um nome e é um arquivo DOCX
-        if file.filename == '':
-            return "Erro: Nome de arquivo inválido.", 400
-        if not file.filename.endswith('.docx'):
-            return "Erro: O arquivo deve ser DOCX.", 400
-
-        # Salva o arquivo DOCX em um local temporário
-        docx_filename = secure_filename(file.filename)
-        docx_path = os.path.join(app.root_path, 'uploads', docx_filename)
-        file.save(docx_path)
-
-        # Caminho para o arquivo de saída PDF
-        pdf_filename = f"{os.path.splitext(docx_filename)[0]}.pdf"
-        pdf_path = os.path.join(app.root_path, 'downloads', pdf_filename)
-
-        # Converte o arquivo DOCX para PDF
-        convert(docx_path, pdf_path)
-
-        # Envia o arquivo PDF convertido como resposta
-        return send_file(pdf_path, as_attachment=True)
+		res.status(200).send('Sucesso');
 	} catch (error) {
 		console.error(error);
 		res.status(500).send('Error adding user');
