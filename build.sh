7ab63488b44e8e666aafe31f4f190c37b1a8223c
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-06-24 22:34:32 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-apt-get update
-apt-get install -y wget
+
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
 dpkg -i wkhtmltox_0.12.6-1.bionic_amd64.deb
 apt-get -f install -y
